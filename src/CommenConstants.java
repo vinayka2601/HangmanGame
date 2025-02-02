bfb540d5fce8c2202ec48bb307a564c3e90447fa
vinayka2601 <vinaykaawasthi26@gmail.com>
2025-02-02 15:08:01 +0530

    commit

---
+++ src/CommenConstants.java
@@ -0,0 +1,6 @@
+import java.awt.*;
+
+public class CommenConstants {
+
+    public static final Dimension FRAME_SIZE = new Dimension(540, 760);
+}
---
+++ src/Hangman.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Hangman extends JFrame{
+  public Hangman(){
+      super("Hangman Game (Java Ed.)");
+      setSize(CommenConstants.FRAME_SIZE);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      setLocationRelativeTo(null);
+      setLayout(null);
+      setResizable(false);
+    }
+
+}
